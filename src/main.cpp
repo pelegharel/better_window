fe745e3e315ca06c5ee519e975350f26a06b1c1d
Peleg Harel <harel67@gmail.com>
2020-04-09 23:13:30 +0300

    remove bloat

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 
 #include <GLFW/glfw3.h>
 
+#include <algorithm>
 #include <array>
 #include <functional>
 #include <memory>
@@ -21,17 +22,85 @@
 #include <tuple>
 #include <type_traits>
 
-template <typename T, auto destroy_fn>
-using c_unique_ptr =
-    std::unique_ptr<T,
-                    std::integral_constant<decltype(destroy_fn), destroy_fn>>;
-
 static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-void main_loop(c_unique_ptr<GLFWwindow, glfwDestroyWindow> window) {
-  struct {
+struct ImguiContext_glfw_opengl {
+  GLFWwindow *window = nullptr;
+
+  ImguiContext_glfw_opengl() {
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+      exit(1);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", nullptr,
+                              nullptr);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    gladLoadGL((GLADloadfunc)glfwGetProcAddress);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+  }
+
+  ImguiContext_glfw_opengl(const ImguiContext_glfw_opengl &) = delete;
+  ImguiContext_glfw_opengl(ImguiContext_glfw_opengl &&) = delete;
+  ImguiContext_glfw_opengl &
+  operator=(const ImguiContext_glfw_opengl &) = delete;
+
+  ImguiContext_glfw_opengl &operator=(ImguiContext_glfw_opengl &&) = delete;
+
+  void render(ImVec4 clear_color) {
+    ImGui::Render();
+    glfwMakeContextCurrent(window);
+
+    const auto [display_w, display_h] = [&] {
+      int display_w;
+      int display_h;
+      glfwGetFramebufferSize(window, &display_w, &display_h);
+      return std::tuple{display_w, display_h};
+    }();
+
+    const auto [x, y, z, w] = clear_color;
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(x, y, z, w);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwMakeContextCurrent(window);
+    glfwSwapBuffers(window);
+  }
+
+  bool is_window_open() const { return !glfwWindowShouldClose(window); }
+
+  ~ImguiContext_glfw_opengl() {
+    glfwDestroyWindow(window);
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+  }
+};
+
+void start_imgui_frame() {
+  // Start the ImGui frame
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+}
+
+void main_loop(ImguiContext_glfw_opengl &context) {
+  struct State {
     bool show_demo_window = true;
     bool show_theme_window = true;
     bool show_another_window = false;
@@ -41,162 +110,98 @@ void main_loop(c_unique_ptr<GLFWwindow, glfwDestroyWindow> window) {
     int counter = 0;
 
     int theme_current = 0;
-    int old_theme_current = 0;
-  } s;
+  };
 
-  while (!glfwWindowShouldClose(window.get())) {
-    glfwPollEvents();
+  State s;
 
-    // Start the ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+  while (context.is_window_open()) {
+    s = [&context, s] {
+      State ns = s;
 
-    // 1. Show a simple window.
-    // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets
-    // automatically appears in a window called "Debug".
-    {
-      ImGui::Text("Hello, world!"); // Display some text (you can use a format
-                                    // string too)
-      ImGui::SliderFloat("float", &s.f, 0.0f,
-                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3(
-          "clear color",
-          (float *)&s.clear_color); // Edit 3 floats representing a color
+      glfwPollEvents();
+      start_imgui_frame();
 
-      ImGui::Text("Windows");
-      ImGui::Checkbox("Demo Window",
-                      &s.show_demo_window); // Edit bools storing our windows
-                                            // open/close state
-      ImGui::Checkbox("Themes Window",
-                      &s.show_theme_window); // Edit bools storing our windows
-                                             // open/close state
-      ImGui::Checkbox("Another Window", &s.show_another_window);
+      // 1. Show a simple window.
+      // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets
+      // automatically appears in a window called "Debug".
+      [&ns, s] {
+        ImGui::Text("Hello, world!"); // Display some text (you can use a format
+                                      // string too)
+        ImGui::SliderFloat(
+            "float", &ns.f, 0.0f,
+            1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3(
+            "clear color",
+            (float *)&ns.clear_color); // Edit 3 floats representing a color
 
-      if (ImGui::Button("Button")) {
-        s.counter++;
+        ImGui::Text("Windows");
+        ImGui::Checkbox("Demo Window",
+                        &ns.show_demo_window); // Edit bools storing our windows
+                                               // open/close state
+        ImGui::Checkbox("Themes Window",
+                        &ns.show_theme_window); // Edit bools storing our
+                                                // windows open/close state
+        ImGui::Checkbox("Another Window", &ns.show_another_window);
+
+        if (ImGui::Button("Button")) {
+          ns.counter++;
+        }
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", s.counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                    1000.0f / ImGui::GetIO().Framerate,
+                    ImGui::GetIO().Framerate);
+      }();
+
+      // 2. Show another simple window. In most cases you will use an explicit
+      // Begin/End pair to name your windows.
+      if (s.show_another_window) {
+        ImGui::Begin("Another Window", &ns.show_another_window);
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me")) {
+          ns.show_another_window = false;
+        }
+        ImGui::End();
       }
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", s.counter);
 
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                  1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    }
+      // 3. Show theme window
+      if (s.show_theme_window) {
+        ImGui::Begin("Themes", &ns.show_theme_window);
+        {
+          const std::array themes = {"Light", "Dark", "Default"};
 
-    // 2. Show another simple window. In most cases you will use an explicit
-    // Begin/End pair to name your windows.
-    if (s.show_another_window) {
-      ImGui::Begin("Another Window", &s.show_another_window);
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me"))
-        s.show_another_window = false;
-      ImGui::End();
-    }
+          constexpr std::array<void (*)(ImGuiStyle *), size(themes)> funcs{
+              {ImGui::StyleColorsLight, ImGui::StyleColorsDark,
+               ImGui::StyleColorsClassic}};
 
-    // 3. Show theme window
-    if (s.show_theme_window) {
-      ImGui::Begin("Themes", &s.show_theme_window);
-      {
-        const std::array themes = {"Light", "Dark", "Default"};
+          ImGui::Combo("theme-combo", &ns.theme_current, themes.data(),
+                       themes.size());
 
-        constexpr std::array<void (*)(ImGuiStyle *), size(themes)> funcs{
-            {ImGui::StyleColorsLight, ImGui::StyleColorsDark,
-             ImGui::StyleColorsClassic}};
-
-        ImGui::Combo("theme-combo", &s.theme_current, themes.data(),
-                     themes.size());
-        if (s.old_theme_current != s.theme_current) {
-          s.old_theme_current = s.theme_current;
           funcs[s.theme_current](nullptr);
         }
+
+        ImGui::End();
       }
 
-      ImGui::End();
-    }
+      // 4. Show the ImGui demo window. Most of the sample code is in
+      // ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
+      if (s.show_demo_window) {
+        ImGui::ShowDemoWindow(&ns.show_demo_window);
+      }
 
-    // 4. Show the ImGui demo window. Most of the sample code is in
-    // ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-    if (s.show_demo_window) {
-      ImGui::SetNextWindowPos(
-          ImVec2(650, 20),
-          ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to
-                                   // call this because positions are saved in
-                                   // .ini file anyway. Here we just want to
-                                   // make the demo initial state a bit more
-                                   // friendly!
-      ImGui::ShowDemoWindow(&s.show_demo_window);
-    }
+      context.render(s.clear_color);
 
-    // ImGui::PopFont();
-    // Rendering
-    ImGui::Render();
-    glfwMakeContextCurrent(window.get());
-
-    const auto [display_w, display_h] = [&window] {
-      int display_w;
-      int display_h;
-      glfwGetFramebufferSize(window.get(), &display_w, &display_h);
-      return std::tuple{display_w, display_h};
+      return ns;
     }();
-
-    {
-      const auto [x, y, z, w] = s.clear_color;
-      glViewport(0, 0, display_w, display_h);
-      glClearColor(x, y, z, w);
-      glClear(GL_COLOR_BUFFER_BIT);
-    }
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-    glfwMakeContextCurrent(window.get());
-    glfwSwapBuffers(window.get());
   }
-}
-
-void setup_imgui_bindins(c_unique_ptr<GLFWwindow, glfwDestroyWindow> &window) {
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  (void)io;
-
-  ImGui_ImplGlfw_InitForOpenGL(window.get(), true);
-  ImGui_ImplOpenGL3_Init();
-}
-
-void cleanup() {
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  ImGui::DestroyContext();
-
-  glfwTerminate();
 }
 
 int main(int, char **) {
 
-  c_unique_ptr<GLFWwindow, glfwDestroyWindow> window{[] {
-    glfwSetErrorCallback(glfw_error_callback);
+  ImguiContext_glfw_opengl context;
 
-    if (!glfwInit()) {
-      exit(1);
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    return glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", nullptr,
-                            nullptr);
-  }()};
-
-  glfwMakeContextCurrent(window.get());
-  glfwSwapInterval(1);
-
-  gladLoadGL((GLADloadfunc)glfwGetProcAddress);
-
-  setup_imgui_bindins(window);
-  ImGui::StyleColorsLight();
-
-  main_loop(std::move(window));
-
-  cleanup();
+  main_loop(context);
 
   return 0;
 }
