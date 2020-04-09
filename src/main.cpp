b76729737f1f9292444f90ddc468f6b30f52a766
Peleg Harel <harel67@gmail.com>
2020-04-09 20:20:47 +0300

    remove fonts

--- src/main.cpp
+++ src/main.cpp
@@ -14,114 +14,61 @@
 
 #include <GLFW/glfw3.h>
 
+#include <array>
+#include <functional>
 #include <memory>
 #include <stdio.h>
+#include <tuple>
 #include <type_traits>
 
-// template <typename T, auto destroy_fn>
-// using c_unique_ptr =
-//    std::unique_ptr<T, std::integral_constant<decltype(fn), fn>>;
+template <typename T, auto destroy_fn>
+using c_unique_ptr =
+    std::unique_ptr<T,
+                    std::integral_constant<decltype(destroy_fn), destroy_fn>>;
 
 static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-void StyleColorsSofty(ImGuiStyle *dst = NULL);
-
-void setup_window() {
-  glfwSetErrorCallback(glfw_error_callback);
-
-  if (!glfwInit()) {
-    exit(1);
-  }
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-}
-
 int main(int, char **) {
-  setup_window();
 
-  //  auto www = c_unique_ptr<GLFWwindow, glfwDestroyWindow(window)> {
-  //    glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", NULL, NULL);
-  //  };
+  c_unique_ptr<GLFWwindow, glfwDestroyWindow> window{[] {
+    glfwSetErrorCallback(glfw_error_callback);
 
-  GLFWwindow *window =
-      glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", NULL, NULL);
-  glfwMakeContextCurrent(window);
+    if (!glfwInit()) {
+      exit(1);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    return glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL3 example", NULL,
+                            NULL);
+  }()};
+
+  glfwMakeContextCurrent(window.get());
   glfwSwapInterval(1); // Enable vsync
 
   gladLoadGL((GLADloadfunc)glfwGetProcAddress);
-
-  // gl3wInit();
 
   // Setup Dear ImGui binding
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGuiIO &io = ImGui::GetIO();
   (void)io;
-  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard
-  // Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable
-  // Gamepad Controls
 
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplGlfw_InitForOpenGL(window.get(), true);
   ImGui_ImplOpenGL3_Init();
-
-  // Setup style
-  // ImGui::StyleColorsDark();
-  // ImGui::StyleColorsClassic();
   ImGui::StyleColorsLight();
 
-  // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can
-  // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
-  // them.
-  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
-  // need to select the font among multiple.
-  // - If the file cannot be loaded, the function will return NULL. Please
-  // handle those errors in your application (e.g. use an assertion, or display
-  // an error and quit).
-  // - The fonts will be rasterized at a given size (w/ oversampling) and stored
-  // into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which
-  // ImGui_ImplXXXX_NewFrame below will call.
-  // - Read 'misc/fonts/README.txt' for more instructions and details.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string
-  // literal you need to write a double backslash \\ !
-  // io.Fonts->AddFontDefault();
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
 
-  auto font_default = io.Fonts->AddFontDefault();
-  auto font_cousine = io.Fonts->AddFontFromFileTTF(
-      CPP_SRC_DIR "modules/imgui/misc/fonts/Cousine-Regular.ttf", 15.0f);
-  auto font_karla = io.Fonts->AddFontFromFileTTF(
-      CPP_SRC_DIR "modules/imgui/misc/fonts/Karla-Regular.ttf", 18.0f);
-  auto font_lato =
-      io.Fonts->AddFontFromFileTTF(CPP_SRC_DIR "fonts/Lato-Regular.ttf", 18.0f);
-
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-  // ImFont* font =
-  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
-  // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
   bool show_demo_window = true;
   bool show_theme_window = true;
   bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-  // Main loop
-  while (!glfwWindowShouldClose(window)) {
-    // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
-    // tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to
-    // your main application.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
-    // data to your main application. Generally you may always pass all inputs
-    // to dear imgui, and hide them from your application based on those two
-    // flags.
+  while (!glfwWindowShouldClose(window.get())) {
     glfwPollEvents();
 
     // Start the ImGui frame
@@ -129,22 +76,6 @@ int main(int, char **) {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    static int font_current = 0;
-    static int old_font_current = 0;
-    switch (font_current) {
-    case 0:
-      ImGui::PushFont(font_default);
-      break;
-    case 1:
-      ImGui::PushFont(font_cousine);
-      break;
-    case 2:
-      ImGui::PushFont(font_karla);
-      break;
-    case 3:
-      ImGui::PushFont(font_lato);
-      break;
-    }
 
     // 1. Show a simple window.
     // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets
@@ -169,21 +100,21 @@ int main(int, char **) {
                                            // open/close state
       ImGui::Checkbox("Another Window", &show_another_window);
 
-      ImGui::Text("Font Samples");
-      ImGui::PushFont(font_cousine);
-      ImGui::Text("Font Render Test - Cousine: Bit Test.123");
-      ImGui::Text("Font Render Test - Cousine: XXXXXXXXXXXX");
-      ImGui::PopFont();
+ //     ImGui::Text("Font Samples");
+ //     ImGui::PushFont(font_cousine);
+ //     ImGui::Text("Font Render Test - Cousine: Bit Test.123");
+ //     ImGui::Text("Font Render Test - Cousine: XXXXXXXXXXXX");
+ //     ImGui::PopFont();
 
-      ImGui::PushFont(font_karla);
-      ImGui::Text("Font Render Test - Karla: Bit Test.123");
-      ImGui::Text("Font Render Test - Karla: XXXXXXXXXXXX");
-      ImGui::PopFont();
+ //     ImGui::PushFont(font_karla);
+ //     ImGui::Text("Font Render Test - Karla: Bit Test.123");
+ //     ImGui::Text("Font Render Test - Karla: XXXXXXXXXXXX");
+ //     ImGui::PopFont();
 
-      ImGui::PushFont(font_lato);
-      ImGui::Text("Font Render Test - Lato: Bit Test.123");
-      ImGui::Text("Font Render Test - Lato: XXXXXXXXXXXX");
-      ImGui::PopFont();
+ //     ImGui::PushFont(font_lato);
+ //     ImGui::Text("Font Render Test - Lato: Bit Test.123");
+ //     ImGui::Text("Font Render Test - Lato: XXXXXXXXXXXX");
+ //     ImGui::PopFont();
 
       if (ImGui::Button("Button")) // Buttons return true when clicked (NB: most
                                    // widgets return true when edited/activated)
@@ -209,38 +140,21 @@ int main(int, char **) {
     if (show_theme_window) {
       ImGui::Begin("Themes", &show_theme_window);
       {
-        // Using the _simplified_ one-liner Combo() api here
-        // See "Combo" section for examples of how to use the more complete
-        // BeginCombo()/EndCombo() api.
-        const char *themes[] = {"Light", "Dark", "Default", "Softy"};
+        const std::array themes = {"Light", "Dark", "Default"};
+
+        constexpr std::array<void (*)(ImGuiStyle *), 3> funcs{
+            {ImGui::StyleColorsLight, ImGui::StyleColorsDark,
+             ImGui::StyleColorsClassic}};
+
         static int theme_current = 0;
         static int old_theme_current = 0;
-        ImGui::Combo("theme-combo", &theme_current, themes,
-                     IM_ARRAYSIZE(themes));
+        ImGui::Combo("theme-combo", &theme_current, themes.data(),
+                     themes.size());
         if (old_theme_current != theme_current) {
           old_theme_current = theme_current;
-
-          switch (theme_current) {
-          case 0:
-            ImGui::StyleColorsLight();
-            break;
-          case 1:
-            ImGui::StyleColorsDark();
-            break;
-          case 2:
-            ImGui::StyleColorsClassic();
-            break;
-          case 3:
-            StyleColorsSofty();
-            break;
-          }
+          funcs[theme_current](nullptr);
         }
 
-        const char *fonts[] = {"Default", "Cousine", "Karla", "Lato"};
-        ImGui::Combo("font-combo", &font_current, fonts, IM_ARRAYSIZE(fonts));
-        if (old_font_current != font_current) {
-          old_font_current = font_current;
-        }
       }
 
       ImGui::End();
@@ -259,19 +173,28 @@ int main(int, char **) {
       ImGui::ShowDemoWindow(&show_demo_window);
     }
 
-    ImGui::PopFont();
+    //ImGui::PopFont();
     // Rendering
     ImGui::Render();
-    int display_w, display_h;
-    glfwMakeContextCurrent(window);
-    glfwGetFramebufferSize(window, &display_w, &display_h);
-    glViewport(0, 0, display_w, display_h);
-    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glfwMakeContextCurrent(window.get());
+
+    const auto [display_w, display_h] = [&window] {
+      int display_w;
+      int display_h;
+      glfwGetFramebufferSize(window.get(), &display_w, &display_h);
+      return std::tuple{display_w, display_h};
+    }();
+
+    {
+      const auto [x, y, z, w] = clear_color;
+      glViewport(0, 0, display_w, display_h);
+      glClearColor(x, y, z, w);
+      glClear(GL_COLOR_BUFFER_BIT);
+    }
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-    glfwMakeContextCurrent(window);
-    glfwSwapBuffers(window);
+    glfwMakeContextCurrent(window.get());
+    glfwSwapBuffers(window.get());
   }
 
   // Cleanup
@@ -279,82 +202,7 @@ int main(int, char **) {
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
 
-  glfwDestroyWindow(window);
   glfwTerminate();
 
   return 0;
-}
-
-// Theme from: https://github.com/ocornut/imgui/issues/707
-//
-void StyleColorsSofty(ImGuiStyle *dst) {
-  ImGuiStyle *style = dst ? dst : &ImGui::GetStyle();
-
-  int hspacing = 8;
-  int vspacing = 6;
-  style->DisplaySafeAreaPadding = ImVec2(0, 0);
-  style->WindowPadding = ImVec2(hspacing / 2, vspacing);
-  style->FramePadding = ImVec2(hspacing, vspacing);
-  style->ItemSpacing = ImVec2(hspacing, vspacing);
-  style->ItemInnerSpacing = ImVec2(hspacing, vspacing);
-  style->IndentSpacing = 20.0f;
-
-  style->WindowRounding = 0.0f;
-  style->FrameRounding = 0.0f;
-
-  style->WindowBorderSize = 0.0f;
-  style->FrameBorderSize = 1.0f;
-  style->PopupBorderSize = 1.0f;
-
-  style->ScrollbarSize = 20.0f;
-  style->ScrollbarRounding = 0.0f;
-  style->GrabMinSize = 5.0f;
-  style->GrabRounding = 0.0f;
-
-  ImVec4 white = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
-  ImVec4 transparent = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-  ImVec4 dark = ImVec4(0.00f, 0.00f, 0.00f, 0.20f);
-  ImVec4 darker = ImVec4(0.00f, 0.00f, 0.00f, 0.50f);
-
-  ImVec4 background = ImVec4(0.95f, 0.95f, 0.95f, 1.00f);
-  ImVec4 text = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
-  ImVec4 border = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
-  ImVec4 grab = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
-  ImVec4 header = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
-  ImVec4 active = ImVec4(0.00f, 0.47f, 0.84f, 1.00f);
-  ImVec4 hover = ImVec4(0.00f, 0.47f, 0.84f, 0.20f);
-
-  style->Colors[ImGuiCol_Text] = text;
-  style->Colors[ImGuiCol_WindowBg] = background;
-  style->Colors[ImGuiCol_ChildBg] = background;
-  style->Colors[ImGuiCol_PopupBg] = white;
-
-  style->Colors[ImGuiCol_Border] = border;
-  style->Colors[ImGuiCol_BorderShadow] = transparent;
-
-  style->Colors[ImGuiCol_Button] = header;
-  style->Colors[ImGuiCol_ButtonHovered] = hover;
-  style->Colors[ImGuiCol_ButtonActive] = active;
-
-  style->Colors[ImGuiCol_FrameBg] = white;
-  style->Colors[ImGuiCol_FrameBgHovered] = hover;
-  style->Colors[ImGuiCol_FrameBgActive] = active;
-
-  style->Colors[ImGuiCol_MenuBarBg] = header;
-  style->Colors[ImGuiCol_Header] = header;
-  style->Colors[ImGuiCol_HeaderHovered] = hover;
-  style->Colors[ImGuiCol_HeaderActive] = active;
-
-  style->Colors[ImGuiCol_CheckMark] = text;
-  style->Colors[ImGuiCol_SliderGrab] = grab;
-  style->Colors[ImGuiCol_SliderGrabActive] = darker;
-
-  // style->Colors[ImGuiCol_CloseButton] = transparent;
-  // style->Colors[ImGuiCol_CloseButtonHovered] = transparent;
-  // style->Colors[ImGuiCol_CloseButtonActive] = transparent;
-
-  style->Colors[ImGuiCol_ScrollbarBg] = header;
-  style->Colors[ImGuiCol_ScrollbarGrab] = grab;
-  style->Colors[ImGuiCol_ScrollbarGrabHovered] = dark;
-  style->Colors[ImGuiCol_ScrollbarGrabActive] = darker;
 }
